e186dca93494a8d5016047eef6d2ca4411e71106
Lucas12j <lucasdasilvadossantos12j@gmail.com>
2019-12-11 22:11:22 -0300

    fist version

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from library import processo
 import csv
 
 while True:
-    start = processo.Processo('E:\Projetos\Jarvis\Dialogos.csv')   #(Para Windows) Coloque o local do arquivo "Dialogo.csv" dentro argumento.
-    #fluxo = processo.Processo('Dialogos.csv')                     #(Outros S.Os)  Aqui não é necessário.           
+    #start = processo.Processo('E:\Projetos\Jarvis\Dialogos.csv')   #(Para Windows) Coloque o local do arquivo "Dialogo.csv" dentro argumento.
+    start = processo.Processo('Dialogos.csv')                     #(Outros S.Os)  Aqui não é necessário.           
     start.inicio()
     
      
