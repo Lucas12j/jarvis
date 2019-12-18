9c610edb871edb95de38dab8b9ab77ee7e8f0a16
Lucas da Silva dos Santos <lucasdasilvadossantos12j@gmail.com>
2019-12-18 01:44:56 +0000

    First Version

--- library/datetime.py
+++ library/datetime.py
@@ -10,7 +10,6 @@ class Datetime():
         return str(self.data.day)+"/"+str(self.data.month)+"/"+str(self.data.year)
     
     def getHora(self):
-        return str(self.hora.hour-3)+":"+str(self.hora.minute)+":"+str(self.hora.second)  
+        return str(self.hora.hour)+":"+str(self.hora.minute)+":"+str(self.hora.second)  
 
-        #Devido ao meu dual boot, no windows as horas ficam erradas, com uma diferença de 3 horas a mais.
         
